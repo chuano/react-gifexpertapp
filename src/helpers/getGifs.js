0c7a195af83011717c3233111bc19a312616501d
Chuano Puchol <chuano@gmail.com>
2020-09-02 19:43:11 +0200

    Uso de https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   const apiKey = 'SSAiCGTiMvEk5w0u73YjsvjGeh16R7hY'
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
   const response = await fetch(url)
   const { data } = await response.json()
   const gifs = data.map((img) => ({
@@ -8,6 +8,6 @@ export const getGifs = async (category) => {
     title: img.title,
     url: img.images.downsized_medium.url
   }))
-  
+
   return gifs
 }
