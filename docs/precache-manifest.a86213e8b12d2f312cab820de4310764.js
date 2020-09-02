5895b494de5c8024eb2761bcb9a734653c3687cb
Chuano Puchol <chuano@gmail.com>
2020-09-02 19:34:08 +0200

    Docs agregado

---
+++ docs/precache-manifest.a86213e8b12d2f312cab820de4310764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1147b82c395ccd8b303f4d9c02e58856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84a649d0bf2e2ab0cf5",
+    "url": "/static/css/main.52c3090c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84a649d0bf2e2ab0cf5",
+    "url": "/static/js/main.17afcc8d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
