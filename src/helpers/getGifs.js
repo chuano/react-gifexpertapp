b5ec4c0398980c9416b9a6ac1c4102e0e6f7fb70
Chuano Puchol <chuano@gmail.com>
2020-09-02 19:32:28 +0200

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+  const apiKey = 'SSAiCGTiMvEk5w0u73YjsvjGeh16R7hY'
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
+  const response = await fetch(url)
+  const { data } = await response.json()
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url
+  }))
+  
+  return gifs
+}
