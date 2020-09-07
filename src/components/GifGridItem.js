c8ae0d889004d42e760d8c2079db1e905d44f26c
Chuano Puchol <chuano@gmail.com>
2020-09-07 19:38:21 +0200

    Add PropTypes

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const GifGridItem = ({ title, url}) => {
+export const GifGridItem = ({ title, url }) => {
   return (
     <div className="card animate__animated animate__fadeIn">
       <p>{title}</p>
       <img src={url} alt={title} />
     </div>
   );
+}
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
